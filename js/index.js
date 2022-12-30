ea49dee3fdd16cb81347843ab99181c9b4e4da34
Usama <nhusain34@gmail.com>
2022-12-30 19:13:26 +0530

    ecommerce template first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function showSearch(){
+    document.getElementById("searchPanel").style.visibility="visible";
+    // document.getElementById("searchPanel").style.transform="translateZ(0)"
+}
+function closeSearch(){
+    // document.getElementById("searchPanel").style.transform="translate3d(0,-12px,0);";
+    document.getElementById("searchPanel").style.visibility="hidden";
+}
