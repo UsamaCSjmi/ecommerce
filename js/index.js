cb73af62aa15252d2eb41e2ceddff20d54cbb1d5
Usama <nhusain34@gmail.com>
2023-01-01 18:24:02 +0530

    Quick View Window integration

--- js/index.js
+++ js/index.js
@@ -4,3 +4,17 @@ function showSearch(){
 function closeSearch(){
     document.getElementById("searchPanel").style.visibility="hidden";
 }
+function closeWindow(){
+    document.getElementById('overlay').classList.remove("open");
+}
+function productQuickView(){
+    document.getElementById('overlay').classList.add("open");
+}
+function changeImage(id){
+    var active=document.getElementsByClassName("active-image");
+    active[0].classList.remove("active-image");
+    var element=document.getElementById(id);
+    element.classList.add("active-image");
+    var src = element.getAttribute('src');
+    document.getElementById("main-image").setAttribute('src',src);
+}
