cb348111cb47507be5a45a84e850fb7bffa1093c
Usama <nhusain34@gmail.com>
2023-01-03 17:22:55 +0530

    Product Window Complete

--- js/index.js
+++ js/index.js
@@ -18,3 +18,22 @@ function changeImage(id){
     var src = element.getAttribute('src');
     document.getElementById("main-image").setAttribute('src',src);
 }
+function decreaseQty(){
+    var qty=document.getElementById("qty").value;
+    if(qty>1)
+        qty--;
+    document.getElementById("qty").value=qty;
+    
+}
+function increasQty(){
+    var qty=document.getElementById("qty").value;
+    qty++;
+    document.getElementById("qty").value=qty;
+}
+function collapseForm(){
+    document.getElementById("arrowBtn").classList.toggle("arrowBtnUp");
+    var doc=document.getElementById("ask-us-inner");
+    doc.classList.toggle("form-open");
+    document.getElementById("window").scrollTo({ left: 0, top: document.getElementById("window").scrollHeight, behavior: "smooth" });
+
+}
